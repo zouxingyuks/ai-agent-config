1e520c496ebfbb0f158e79385136a57c5868a6c2
xyuzou <xyuzou@tencent.com>
2026-06-09 11:24:52 +0000

    feat: Update skills setup and update command

--- skills/skills.sh
+++ skills/skills.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-npx skills update
+npx skills update -g
 npx skills add --agent opencode -g https://github.com/vercel-labs/skills --skill find-skills -y
-npx skills add --agent opencode -g https://github.com/obra/superpowers --skill brainstorming -y
 npx skills add --agent opencode -g https://github.com/mattpocock/skills --skill grill-me -y
+npx skills add --agent opencode -g https://github.com/mattpocock/skills --skill improve-codebase-architecture
 npx skills ls --agent opencode -g

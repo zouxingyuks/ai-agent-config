2dbf6a20d3cadd35d32cb5c3350f4847ef4dca0d
xyuzou <xyuzou@tencent.com>
2026-06-15 08:28:05 +0000

    feat: add the new skill skill-retrospective

--- skills/skills.sh
+++ skills/skills.sh
@@ -3,6 +3,6 @@
 npx skills update -g
 npx skills add --agent opencode -g https://github.com/vercel-labs/skills --skill find-skills -y
 npx skills add --agent opencode -g https://github.com/mattpocock/skills --skill grill-me -y
-npx skills add --agent opencode -g https://github.com/mattpocock/skills --skill improve-codebase-architecture -y
-npx skills add --agent opencode -g https://github.com/jeffallan/claude-skills --skill golang-pro -y
+npx skills add --agent opencode -g https://github.com/mattpocock/skills --skill improve-codebase-architecture
+npx skills add --agent opencode -g https://github.com/zouxingyuks/skills --skill skill-retrospective -y
 npx skills ls --agent opencode -g
